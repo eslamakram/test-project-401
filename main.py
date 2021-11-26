140b8301f20b945c3cc02fd1c79faf9826942ebb
eslamakram <eslam_akram91@yahoo.com>
2021-11-26 17:23:38 +0200

    initial commit

--- main.py
+++ main.py
@@ -12,7 +12,21 @@ def initcal():
 
     calc.protocol("WM_DELETE_WINDOW", on_closing)
 
-myButton2 = Button(root, text="delete", command=initcal)
+myButton2 = Button(root, text="search", command=initcal)
 myButton2.pack()
+from pytube import *
+import urllib.request
+import re
+# ******************************
+# https://www.youtube.com/results?search_query=mozart
+search_keyword = "Physics Explained in Ten Seconds"
+main_search_keyword = search_keyword.replace(' ','')
+html = urllib.request.urlopen("https://www.youtube.com/results?search_query="+main_search_keyword)
+# print(html.read().decode())
+video_ids = re.findall(r"watch\?v=(\S{11})",html.read().decode())
+print(video_ids)
+print("https://www.youtube.com/watch?v="+video_ids[0])
+# ******************************
 
+# returns a dictionary
 root.mainloop()
